d18ccc123cd983b7620e5dc07a4e357db60216ff
Ольга <olga@MacBook-Air-Olga.local>
2024-08-28 10:09:21 +0300

    added Home file

---
+++ src/Home/Woman.js
@@ -0,0 +1,12 @@
+function Woman () {
+    return(
+<div>
+      <img  className='woman' src="https://img.freepik.com/free-photo/portrait-woman-training-home-with-weights_23-2148896494.jpg?w=1800&t=st=1722701960~exp=1722702560~hmac=c0bd58a20e6809afe90e5708542541924b7d01bc4f5ee8dfa073ca4939b0fc87"  width="100%" height="90%" alt='woman'/>
+<div className='onPicture'>
+      <h2 className="boost">Boost your energy level with short, quick workout.</h2>
+      <button className="btnShowPlan">Show plan</button>
+</div>
+</div>
+    )
+}
+export default Woman;
