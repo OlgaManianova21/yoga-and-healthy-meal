3a53b7215f280fb8f9da798b69168cb21b0f1969
OlgaManianova21 <Olgamanianova@gmail.com>
2024-08-28 21:31:30 +0300

    added json and appjs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
+import Footer from './Footer/Footer';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Header from './Header/Header';
+import Home from './Home/Home';
+import Workout from './Exercises/Workout';
+import Recipies from './Recipies/Recipies';
+import ToDOList from './Food/ToDoList';
+
+
 function App() {
-  return (
-    <div className="App">
-     
+
+  return ( <div>
+    <Router>
+
+      <Header/>
+
+    <Routes>
+<Route path= "/" element={<Home />} />
+<Route path= "/food" element={<ToDOList />} />
+<Route path= "/recipies" element={<Recipies />} />
+<Route path= "/exercises" element={<Workout />} />
+   </Routes>
+   </Router>
+
+   
+    <Footer/>
+  
     </div>
-  );
+    );
 }
 
 export default App;
