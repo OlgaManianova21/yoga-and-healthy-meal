cc2c27090e84470976864a6ca717f26043bc13cf
Ольга <olga@MacBook-Air-Olga.local>
2024-08-28 10:16:21 +0300

    added Header files

---
+++ src/Header/Header.js
@@ -0,0 +1,20 @@
+import './Header.css';
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+function Header() {
+
+  return ( 
+    
+   <nav>
+   <Link to="/" className='link'>Home page</Link>
+   <Link to="/food" className='link'>Food</Link>
+   <Link to="/recipies" className='link'>Recipies</Link>
+   <Link to="/exercises" className='link'>Exercises</Link>
+   </nav>
+  
+  );
+}
+
+export default Header;
