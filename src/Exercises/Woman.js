59012fea23af65f92e1644e9f417827fa73e0c3d
OlgaManianova21 <Olgamanianova@gmail.com>
2024-09-15 19:42:26 +0300

    final edits

--- src/Exercises/Woman.js
+++ src/Exercises/Woman.js
@@ -3,8 +3,7 @@ function Woman () {
 <div>
       <img  className='woman' src="https://img.freepik.com/free-photo/portrait-woman-training-home-with-weights_23-2148896494.jpg?w=1800&t=st=1722701960~exp=1722702560~hmac=c0bd58a20e6809afe90e5708542541924b7d01bc4f5ee8dfa073ca4939b0fc87"  width="100%" height="90%" alt='woman'/>
 <div className='onPicture'>
-      <h2 className="boost">Boost your energy level with short, quick workout.</h2>
-      <button className="btnShowPlan">Show plan</button>
+      <h2 className="boost"> This program perfect for those who work out at home, travel and are on the road a lot, or beginners who are new to weight lifting.</h2>
 </div>
 </div>
     )
